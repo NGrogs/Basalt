aaa329e329d4bb98275877be0083542068297d0d
NGrogs <ngrogan15@gmail.com>
2019-01-09 18:29:19 +0000

    added firebase

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,8 @@
+const config = {
+    apiKey: "AIzaSyAgRgoOqFUmvHy022erltHKM0vUHjmexHw",
+    authDomain: "basalt-b3c19.firebaseapp.com",
+    databaseURL: "https://basalt-b3c19.firebaseio.com",
+    projectId: "basalt-b3c19",
+    storageBucket: "basalt-b3c19.appspot.com",
+    messagingSenderId: "446592087044"
+};
