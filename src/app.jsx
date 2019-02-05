bcca9ceb6778228ce5455e8514dc592ce6535b18
NGrogs <ngrogan15@gmail.com>
2019-02-05 12:08:26 +0000

    Added welcome page

--- src/app.jsx
+++ src/app.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import NavigationBar from './components/Nav/NavigationBar';
+import Welcome from './components/Home/welcome';
 import Home from './components/Home/home';
 import Login from './components/Auth/login';
 import SignUp from './components/Auth/signup';
@@ -15,6 +16,7 @@ class App extends Component {
                 <div>
                     <NavigationBar />
                     <Switch>
+                        <Route exact path='/' component={Welcome} />
                         <Route exact path='/home' component={Home} />
                         <Route exact path='/login' component={Login} /> 
                         <Route exact path='/signup' component={SignUp} />
---
+++ src/components/Home/welcome.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class Welcome extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+        return (
+            <div align="center"className="container">
+                <h1> Welcome to Basalt </h1>
+                
+                
+            </div>
+        )
+    }
+}
+
+export default Welcome;
